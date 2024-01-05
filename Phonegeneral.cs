fef2006e9cc6854d93324250c202523325df41f9
thabrkci <alpacinotaha@gmail.com>
2024-01-05 14:51:43 +0300

    first commit

---
+++ Phonegeneral.cs
@@ -0,0 +1,14 @@
+public abstract class Phonegeneral
+{
+  public abstract void Call();//Arama yaptırmak için yazılan abstract geri dönüş tipi olmayan metod.
+  public abstract void SendSms();//Mesaj atmak için yazılan abstract geri dönüş tipi olmayan metod.
+  public abstract void connectwifi();//Wifiye bağlanmak için yazılan abstract geri dönüş tipi olmayan metod.
+  
+  
+
+
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Abstractexample2;
+
+class Program
+{
+    static void Main(string[] args)
+    {   
+        Console.WriteLine("|||Telefonunuz aşağıdaki işlemleri yapıyor.|||"); 
+        Phoneinfo phoneinfo = new();
+        phoneinfo.Call();
+        phoneinfo.SendSms();
+        phoneinfo.connectwifi();
+        
+        Console.WriteLine("|||Nokia Telefonunuz||| \n |||aşağıdaki işlemleri yapıyor.|||");
+        Nokia nokia = new();
+        nokia.Call();
+        nokia.SendSms();
+        nokia.connectwifi();
+        nokia.PlaySnakeGame();
+    }   
+    
+
+    
+}
+
+
+
+public class Phoneinfo : Phonegeneral
+{
+    public override void Call()
+    {
+        Console.WriteLine("Telefon arama yapıyor");
+    }
+    public override void SendSms()
+    {
+        Console.WriteLine("Telefonunun sms gönderiyor");
+
+    }
+    public override void connectwifi()
+    {
+        Console.WriteLine("Telefonunuz internete bağlanıyor");
+    }
+
+
+}
+
+
+
+class Nokia:Phoneinfo//Nokia telefon için tekrar override yapmamıza gerek yoktu,hızlı bir şekilde Nokia nokia = new(); ile phone info sınıfındaki metotları kullanabiliriz.
+{
+        public void PlaySnakeGame()
+        {
+            Console.WriteLine("Nokia telefonunda Yılan Oyunu oynanıyor.");
+        }
+
+    // Phoneinfo sınıfındaki metotları override etmek istersek.
+        public override void Call()
+        {
+         Console.WriteLine("Nokia marka cep telefonunuz arama yapıyor.");
+        }
+        public override void SendSms()
+        {
+            Console.WriteLine("Nokia telefonu sms gönderiyor");
+        }
+
+        // ConnectWifi metodu Phoneinfo sınıfında zaten override edildiği için burada tekrar implemente etmeye gerek yok.
+    }
+
+
+
+
+
+
